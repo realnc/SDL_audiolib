realnc/SDL_audiolib

commit b7fa1a6f07ba95dca7c02f5b1f49f5703dd1db8f

Add more checks in Decoders to prevent accessing invalid handles

src/DecoderFluidsynth.cpp
@@ -150,6 +150,9 @@ Aulib::DecoderFluidsynth::~DecoderFluidsynth() = default;
 
 auto Aulib::DecoderFluidsynth::loadSoundfont(SDL_RWops* rwops) -> bool
 {
+    if (not isOpen()) {
+        return false;
+    }
     if (not rwops) {
         SDL_SetError("rwops is null.");
         return false;
@@ -178,6 +181,9 @@ auto Aulib::DecoderFluidsynth::loadSoundfont(SDL_RWops* rwops) -> bool
 
 auto Aulib::DecoderFluidsynth::loadSoundfont(const std::string& filename) -> bool
 {
+    if (not isOpen()) {
+        return false;
+    }
     if (fluid_synth_sfload(d->fSynth.get(), filename.c_str(), 1) == FLUID_FAILED) {
         SDL_SetError("FluidSynth failed to load soundfont.");
         return false;
@@ -187,12 +193,17 @@ auto Aulib::DecoderFluidsynth::loadSoundfont(const std::string& filename) -> boo
 
 auto Aulib::DecoderFluidsynth::gain() const -> float
 {
+    if (not isOpen()) {
+        return 0.f;
+    }
     return fluid_synth_get_gain(d->fSynth.get());
 }
 
 void Aulib::DecoderFluidsynth::setGain(float gain)
 {
-    fluid_synth_set_gain(d->fSynth.get(), gain);
+    if (isOpen()) {
+        fluid_synth_set_gain(d->fSynth.get(), gain);
+    }
 }
 
 auto Aulib::DecoderFluidsynth::open(SDL_RWops* rwops) -> bool
@@ -240,8 +251,7 @@ auto Aulib::DecoderFluidsynth::open(SDL_RWops* rwops) -> bool
 
 auto Aulib::DecoderFluidsynth::getChannels() const -> int
 {
-    if (not d->fSynth) {
-        SDL_SetError("FluidSynth failed to initialize.");
+    if (not settings) {
         return 0;
     }
 
@@ -253,8 +263,7 @@ auto Aulib::DecoderFluidsynth::getChannels() const -> int
 
 auto Aulib::DecoderFluidsynth::getRate() const -> int
 {
-    if (not d->fSynth) {
-        SDL_SetError("FluidSynth failed to initialize.");
+    if (not settings) {
         return 0;
     }
 
@@ -265,7 +274,7 @@ auto Aulib::DecoderFluidsynth::getRate() const -> int
 
 auto Aulib::DecoderFluidsynth::doDecoding(float buf[], int len, bool& /*callAgain*/) -> int
 {
-    if (not d->fPlayer or d->fEOF) {
+    if (d->fEOF or not isOpen()) {
         return 0;
     }
 
@@ -282,8 +291,7 @@ auto Aulib::DecoderFluidsynth::doDecoding(float buf[], int len, bool& /*callAgai
 
 auto Aulib::DecoderFluidsynth::rewind() -> bool
 {
-    if (not d->fSynth) {
-        SDL_SetError("FluidSynth failed to initialize.");
+    if (not isOpen()) {
         return false;
     }
 

src/DecoderAdlmidi.cpp
@@ -117,7 +117,7 @@ auto Aulib::DecoderAdlmidi::setEmulator(Emulator emulator) -> bool
 {
     d->emulator = emulator;
     d->change_emulator = true;
-    if (not d->adl_player) {
+    if (not isOpen()) {
         return true;
     }
     if (not d->setEmulator()) {
@@ -130,7 +130,7 @@ auto Aulib::DecoderAdlmidi::setEmulator(Emulator emulator) -> bool
 auto Aulib::DecoderAdlmidi::setChipAmount(int chip_amount) -> bool
 {
     d->chip_amount = chip_amount;
-    if (not d->adl_player) {
+    if (not isOpen()) {
         return true;
     }
     if (not d->setChipAmount()) {
@@ -152,7 +152,7 @@ auto Aulib::DecoderAdlmidi::loadBank(SDL_RWops* rwops) -> bool
         return false;
     }
     d->embedded_bank = -1;
-    if (not d->adl_player) {
+    if (not isOpen()) {
         d->bank_data = std::move(tmp_data);
         return true;
     }
@@ -181,7 +181,7 @@ auto Aulib::DecoderAdlmidi::loadEmbeddedBank(int bank_number) -> bool
     }
     d->bank_data.reset();
     d->embedded_bank = bank_number;
-    if (not d->adl_player) {
+    if (not isOpen()) {
         return true;
     }
     if (not d->setEmbeddedBank()) {
@@ -251,7 +251,7 @@ auto Aulib::DecoderAdlmidi::getRate() const -> int
 
 auto Aulib::DecoderAdlmidi::rewind() -> bool
 {
-    if (not d->adl_player) {
+    if (not isOpen()) {
         return false;
     }
     adl_positionRewind(d->adl_player.get());
@@ -266,7 +266,7 @@ auto Aulib::DecoderAdlmidi::duration() const -> chrono::microseconds
 
 auto Aulib::DecoderAdlmidi::seekToTime(chrono::microseconds pos) -> bool
 {
-    if (not d->adl_player) {
+    if (not isOpen()) {
         return false;
     }
     adl_positionSeek(d->adl_player.get(), chrono::duration<double>(pos).count());
@@ -276,7 +276,7 @@ auto Aulib::DecoderAdlmidi::seekToTime(chrono::microseconds pos) -> bool
 
 auto Aulib::DecoderAdlmidi::doDecoding(float buf[], int len, bool& /*callAgain*/) -> int
 {
-    if (not d->adl_player or d->eof) {
+    if (d->eof or not isOpen()) {
         return 0;
     }
     constexpr ADLMIDI_AudioFormat adl_format{ADLMIDI_SampleType_F32, sizeof(float),

src/DecoderBassmidi.cpp
@@ -151,6 +151,10 @@ auto Aulib::DecoderBassmidi::getChannels() const -> int
 
 auto Aulib::DecoderBassmidi::getRate() const -> int
 {
+    if (not isOpen()) {
+        return 0;
+    }
+
     BASS_CHANNELINFO inf;
     if (BASS_ChannelGetInfo(d->hstream.get(), &inf) != 0) {
         return inf.freq;
@@ -162,7 +166,7 @@ auto Aulib::DecoderBassmidi::getRate() const -> int
 
 auto Aulib::DecoderBassmidi::doDecoding(float buf[], int len, bool& /*callAgain*/) -> int
 {
-    if (d->eof or not d->hstream) {
+    if (d->eof or not isOpen()) {
         return 0;
     }
 
@@ -185,7 +189,7 @@ auto Aulib::DecoderBassmidi::rewind() -> bool
 
 auto Aulib::DecoderBassmidi::duration() const -> chrono::microseconds
 {
-    if (not d->hstream) {
+    if (not isOpen()) {
         return {};
     }
 
@@ -207,7 +211,7 @@ auto Aulib::DecoderBassmidi::duration() const -> chrono::microseconds
 
 auto Aulib::DecoderBassmidi::seekToTime(chrono::microseconds pos) -> bool
 {
-    if (not d->hstream) {
+    if (not isOpen()) {
         return false;
     }
 

src/DecoderDrflac.cpp
@@ -91,7 +91,7 @@ auto Aulib::DecoderDrflac::open(SDL_RWops* const rwops) -> bool
 
 auto Aulib::DecoderDrflac::doDecoding(float* const buf, const int len, bool& /*callAgain*/) -> int
 {
-    if (d->fEOF) {
+    if (d->fEOF or not isOpen()) {
         return 0;
     }
 
@@ -105,25 +105,30 @@ auto Aulib::DecoderDrflac::doDecoding(float* const buf, const int len, bool& /*c
 
 auto Aulib::DecoderDrflac::getChannels() const -> int
 {
+    if (not isOpen()) {
+        return 0;
+    }
     return d->handle_.get()->channels;
 }
 
 auto Aulib::DecoderDrflac::getRate() const -> int
 {
+    if (not isOpen()) {
+        return 0;
+    }
     return d->handle_.get()->sampleRate;
 }
 
 auto Aulib::DecoderDrflac::rewind() -> bool
 {
-    if (drflac_seek_to_pcm_frame(d->handle_.get(), 0)) {
-        d->fEOF = false;
-        return true;
-    }
-    return false;
+    return seekToTime({});
 }
 
 auto Aulib::DecoderDrflac::duration() const -> chrono::microseconds
 {
+    if (not isOpen()) {
+        return {};
+    }
     return chrono::duration_cast<chrono::microseconds>(chrono::duration<double>(
         static_cast<double>(d->handle_.get()->totalPCMFrameCount) / getRate()));
 }
@@ -131,7 +136,7 @@ auto Aulib::DecoderDrflac::duration() const -> chrono::microseconds
 auto Aulib::DecoderDrflac::seekToTime(const chrono::microseconds pos) -> bool
 {
     const auto target_frame = chrono::duration<double>(pos).count() * getRate();
-    if (not drflac_seek_to_pcm_frame(d->handle_.get(), target_frame)) {
+    if (not isOpen() or not drflac_seek_to_pcm_frame(d->handle_.get(), target_frame)) {
         return false;
     }
     d->fEOF = false;

src/DecoderDrwav.cpp
@@ -96,7 +96,7 @@ auto Aulib::DecoderDrwav::open(SDL_RWops* const rwops) -> bool
 
 auto Aulib::DecoderDrwav::doDecoding(float* const buf, const int len, bool& /*callAgain*/) -> int
 {
-    if (d->fEOF) {
+    if (d->fEOF or not isOpen()) {
         return 0;
     }
 
@@ -125,6 +125,9 @@ auto Aulib::DecoderDrwav::rewind() -> bool
 
 auto Aulib::DecoderDrwav::duration() const -> chrono::microseconds
 {
+    if (not isOpen()) {
+        return {};
+    }
     return chrono::duration_cast<chrono::microseconds>(
         chrono::duration<double>(static_cast<double>(d->handle_.totalPCMFrameCount) / getRate()));
 }
@@ -132,7 +135,7 @@ auto Aulib::DecoderDrwav::duration() const -> chrono::microseconds
 auto Aulib::DecoderDrwav::seekToTime(const chrono::microseconds pos) -> bool
 {
     const auto target_frame = chrono::duration<double>(pos).count() * getRate();
-    if (not drwav_seek_to_pcm_frame(&d->handle_, target_frame)) {
+    if (not isOpen() or not drwav_seek_to_pcm_frame(&d->handle_, target_frame)) {
         return false;
     }
     d->fEOF = false;

src/DecoderModplug.cpp
@@ -90,7 +90,7 @@ auto Aulib::DecoderModplug::getRate() const -> int
 
 auto Aulib::DecoderModplug::doDecoding(float buf[], int len, bool& /*callAgain*/) -> int
 {
-    if (d->atEOF) {
+    if (d->atEOF or not isOpen()) {
         return 0;
     }
     Buffer<Sint32> tmpBuf(len);
@@ -117,6 +117,9 @@ auto Aulib::DecoderModplug::duration() const -> chrono::microseconds
 
 auto Aulib::DecoderModplug::seekToTime(chrono::microseconds pos) -> bool
 {
+    if (not isOpen()) {
+        return false;
+    }
     ModPlug_Seek(d->mpHandle.get(), chrono::duration_cast<chrono::milliseconds>(pos).count());
     d->atEOF = false;
     return true;

src/DecoderMpg123.cpp
@@ -136,7 +136,7 @@ auto Aulib::DecoderMpg123::getRate() const -> int
 
 auto Aulib::DecoderMpg123::doDecoding(float buf[], int len, bool& callAgain) -> int
 {
-    if (d->fEOF) {
+    if (d->fEOF or not isOpen()) {
         return 0;
     }
 
@@ -165,7 +165,7 @@ auto Aulib::DecoderMpg123::doDecoding(float buf[], int len, bool& callAgain) -> 
 
 auto Aulib::DecoderMpg123::rewind() -> bool
 {
-    if (mpg123_seek(d->fMpgHandle.get(), 0, SEEK_SET) < 0) {
+    if (not isOpen() or mpg123_seek(d->fMpgHandle.get(), 0, SEEK_SET) < 0) {
         return false;
     }
     d->fEOF = false;
@@ -180,6 +180,11 @@ auto Aulib::DecoderMpg123::duration() const -> chrono::microseconds
 auto Aulib::DecoderMpg123::seekToTime(chrono::microseconds pos) -> bool
 {
     using std::chrono::duration;
+
+    if (not isOpen()) {
+        return false;
+    }
+
     off_t targetFrame = mpg123_timeframe(d->fMpgHandle.get(), duration<double>(pos).count());
     if (targetFrame < 0 or mpg123_seek_frame(d->fMpgHandle.get(), targetFrame, SEEK_SET) < 0) {
         return false;

src/DecoderMusepack.cpp
@@ -85,22 +85,22 @@ auto Aulib::DecoderMusepack::open(SDL_RWops* rwops) -> bool
 
 auto Aulib::DecoderMusepack::getChannels() const -> int
 {
-    return d->demuxer ? d->strmInfo.channels : 0;
+    return d->strmInfo.channels;
 }
 
 auto Aulib::DecoderMusepack::getRate() const -> int
 {
-    return d->demuxer ? d->strmInfo.sample_freq : 0;
+    return d->strmInfo.sample_freq;
 }
 
 auto Aulib::DecoderMusepack::doDecoding(float buf[], int len, bool& /*callAgain*/) -> int
 {
-    int totalSamples = 0;
-    int wantedSamples = len;
-
-    if (d->eof) {
+    if (d->eof or not isOpen()) {
         return 0;
     }
+
+    int totalSamples = 0;
+    int wantedSamples = len;
 
     // If we have any left-over samples from the previous frame, copy them out.
     if (d->curFrame.samples > 0) {
@@ -147,11 +147,12 @@ auto Aulib::DecoderMusepack::rewind() -> bool
 
 auto Aulib::DecoderMusepack::duration() const -> chrono::microseconds
 {
-    if (not d->demuxer) {
-        return chrono::microseconds::zero();
-    }
     using namespace std::chrono;
     using std::chrono::duration;
+
+    if (not isOpen()) {
+        return microseconds::zero();
+    }
     return duration_cast<microseconds>(duration<double>(mpc_streaminfo_get_length(&d->strmInfo)));
 }
 
@@ -159,7 +160,7 @@ auto Aulib::DecoderMusepack::seekToTime(chrono::microseconds pos) -> bool
 {
     using namespace std::chrono;
     using std::chrono::duration;
-    if (not d->demuxer
+    if (not isOpen()
         or mpc_demux_seek_second(d->demuxer.get(), duration<double>(pos).count())
                != MPC_STATUS_OK) {
         return false;

commit 095e2c5477a4eaffb8dab431c407ea67052bd38e

Fix DecoderFluidsynth not playing anything
Commit b7fa1a6f07ba95dca7c02f5b1f49f5703dd1db8f broke it.

Unlike the player, the synth is created during construction, not during
open(). So exiting from loadSoundfont() (or other functions that only
access the synth but not the player) when isOpen() == false made it
impossible to do anything with this decoder.

src/DecoderFluidsynth.cpp
@@ -150,7 +150,7 @@ Aulib::DecoderFluidsynth::~DecoderFluidsynth() = default;
 
 auto Aulib::DecoderFluidsynth::loadSoundfont(SDL_RWops* rwops) -> bool
 {
-    if (not isOpen()) {
+    if (not d->fSynth) {
         return false;
     }
     if (not rwops) {
@@ -181,7 +181,7 @@ auto Aulib::DecoderFluidsynth::loadSoundfont(SDL_RWops* rwops) -> bool
 
 auto Aulib::DecoderFluidsynth::loadSoundfont(const std::string& filename) -> bool
 {
-    if (not isOpen()) {
+    if (not d->fSynth) {
         return false;
     }
     if (fluid_synth_sfload(d->fSynth.get(), filename.c_str(), 1) == FLUID_FAILED) {
@@ -193,7 +193,7 @@ auto Aulib::DecoderFluidsynth::loadSoundfont(const std::string& filename) -> boo
 
 auto Aulib::DecoderFluidsynth::gain() const -> float
 {
-    if (not isOpen()) {
+    if (not d->fSynth) {
         return 0.f;
     }
     return fluid_synth_get_gain(d->fSynth.get());
@@ -201,7 +201,7 @@ auto Aulib::DecoderFluidsynth::gain() const -> float
 
 void Aulib::DecoderFluidsynth::setGain(float gain)
 {
-    if (isOpen()) {
+    if (not d->fSynth) {
         fluid_synth_set_gain(d->fSynth.get(), gain);
     }
 }